4da3e08787fce3b634a6f71cdb81d2d341f19cd4
Florian PASTEUR <florian.c.p@pm.me>
2025-10-20 22:30:15 +0200

    merge kml to gpx

--- src/add-kml-to-gpx.mjs
+++ src/add-kml-to-gpx.mjs
@@ -1,6 +1,69 @@
+import {readXml} from "../lib/read-xml.mjs";
+import {serialize} from "../lib/jsdom-serialize.mjs";
+import {getClosestPoint} from "../lib/closet-point.mjs";
+import {createWaypoint} from "../lib/create-waypoint.mjs";
 
-export function addKmlToGpx(gpx, kml) {
+/**
+ * Add KML placemarks as waypoints to GPX content.
+ * @param gpx string
+ * @param kml string
+ * @param kmlPointType string
+ * @param thresholdDistance number
+ * @returns {Promise<string>}
+ */
+export async function addKmlToGpx(gpx, kml, kmlPointType, thresholdDistance = 300) {
+    const placemarks = await processKml(kml, kmlPointType);
+    const {document: gpxDoc, jsdomRef: gpxJsdomRef, allGpxPoints} = await processGpx(gpx);
 
 
-    return gpx
+    placemarks.forEach((placemark) => {
+        const type = placemark.getAttribute('type');
+        const nameElement = placemark.querySelector('name');
+        const name = nameElement ? nameElement.textContent.trim() : type;
+        const [lon, lat] = placemark.querySelector('coordinates').textContent.trim().split(',').map(parseFloat);
+
+        const closestPoint = getClosestPoint({lat, lon}, allGpxPoints);
+        if (closestPoint.distance < thresholdDistance) {
+            createWaypoint(gpxDoc, {
+                lat: closestPoint.point.lat,
+                lon: closestPoint.point.lon,
+                name: `${name} (${Math.round(closestPoint.distance)}m,${closestPoint.direction})`,
+                type
+            });
+        }
+    });
+
+    return serialize(gpxJsdomRef);
+}
+
+/**
+ * Process KML content and set point types.
+ * @param kmlContent string
+ * @param pointType string
+ * @returns {Promise<Element[]>}
+ */
+async function processKml(kmlContent, pointType) {
+    const {document: kmlDoc} = await readXml(kmlContent);
+
+    return Array.from(kmlDoc.querySelectorAll('Placemark')).map(placemark => {
+        placemark.setAttribute('type', pointType);
+        return placemark;
+    });
+}
+
+
+async function processGpx(gpxContent) {
+    const gpx = await readXml(gpxContent);
+
+
+    const allGpxPoints = Array.from(gpx.document.querySelectorAll('trkpt')).map(trkpt => {
+        const lat = parseFloat(trkpt.getAttribute('lat').trim());
+        const lon = parseFloat(trkpt.getAttribute('lon').trim());
+        return {lat, lon};
+    })
+
+    return {
+        ...gpx,
+        allGpxPoints
+    }
 }
--- tests/test.mjs
+++ tests/test.mjs
@@ -1,5 +1,7 @@
 import * as fs from 'fs/promises';
 import {refineWaypointForGarminConnect} from "../src/refine-waypoints-for-garmin-connect.mjs";
+import {addKmlToGpx} from "../src/add-kml-to-gpx.mjs";
+import {POIType} from "../lib/poi-type.mjs";
 
 ;(async function () {
     const gpxContent = (await fs.readFile('tests/assets/gpx-with-poi.gpx', 'utf-8')).toString();
@@ -7,4 +9,13 @@ import {refineWaypointForGarminConnect} from "../src/refine-waypoints-for-garmin
     const gpxContentWithRefinedPoi = await refineWaypointForGarminConnect(gpxContent);
 
     await fs.writeFile('tests/outputs/gpx-with-refined-poi.gpx', gpxContentWithRefinedPoi);
+})();
+
+;(async function () {
+    const gpxContent = (await fs.readFile('tests/assets/initial-gpx.gpx', 'utf-8')).toString();
+    const kmlContent = (await fs.readFile('tests/assets/poi-map.kml', 'utf-8')).toString();
+
+    const gpxContentWithPoi = await addKmlToGpx(gpxContent, kmlContent, POIType.OVERLOOK, 2000);
+
+    await fs.writeFile('tests/outputs/initial-gpx-with-poi.gpx', gpxContentWithPoi);
 })();
